7077801bb52ca2890f8989455f1e5f30273908d3
Adrian <adrian.jaczynski@gmail.com>
2019-09-22 21:55:14 +0200

    fixed timezone

--- src/main/java/pl/packagemanagement/PackageManagementApplication.java
+++ src/main/java/pl/packagemanagement/PackageManagementApplication.java
@@ -3,8 +3,15 @@ package pl.packagemanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class PackageManagementApplication {
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Etc/UTC")); //ustawienie strefy czasowej dla daty !!
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PackageManagementApplication.class, args);
